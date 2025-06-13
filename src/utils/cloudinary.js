fbaf29d61505b2ada76165de3206656eb855cb67
Harsh <harsh0030sahu@gmail.comm>
2025-06-13 21:58:28 +0530

    add cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "deloi6s5p",
-  api_key: "412422891469958",
-  api_secret: "eWRjI-sjtYVXtjPAWCg5PboW2Zg",
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
